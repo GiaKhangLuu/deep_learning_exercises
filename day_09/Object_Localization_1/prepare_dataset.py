de0fc8111029ec6f58e6ab329133e90169cd4a5c
GiaKhangLuu <waoknight@gmail.com>
2025-03-22 12:37:29 +0700

    update prepare_dataset

--- day_09/Object_Localization_1/prepare_dataset.py
+++ day_09/Object_Localization_1/prepare_dataset.py
@@ -12,15 +12,27 @@ def array_to_tuple(arr):
 # Function to process each folder
 def process_folder(folder_path, output_dir):
     folder_name = os.path.basename(folder_path)
-    classes = ['airplane', 'face', 'motorcycle']
+    classes = ['airplane', 'face', 'motorcycle', 'accordion', 'ant', 'buddha', 'camera', 'octopus']
     target_class = None
 
     if folder_name == 'Airplanes_Side_2':
         target_class = classes[0]
     elif folder_name == 'Faces_2':
         target_class = classes[1]
-    else:
+    elif folder_name == 'Motorbikes_16':
         target_class = classes[2]
+    elif folder_name == 'accordion':
+        target_class = classes[3]
+    elif folder_name == 'ant':
+        target_class = classes[4]
+    elif folder_name == 'buddha':
+        target_class = classes[5]
+    elif folder_name == 'camera':
+        target_class = classes[6]
+    elif folder_name == 'octopus':
+        target_class = classes[7]
+    else:
+        raise ValueError('Invalid folder name')
 
     csv_file_path = output_dir / (target_class + '.csv')
     files = glob(str(folder_path / '*.mat'))
@@ -44,7 +56,7 @@ def process_folder(folder_path, output_dir):
 
 # Main function to iterate through all folders
 def main(input_dir, output_dir):
-    objects_used = ['Airplanes_Side_2', 'Faces_2', 'Motorbikes_16']
+    objects_used = ['Airplanes_Side_2', 'Faces_2', 'Motorbikes_16', 'accordion', 'ant', 'buddha', 'camera', 'octopus']
     for object in tqdm(objects_used):
         older_path = os.path.join(input_dir, object)
         folder_path = input_dir / object
